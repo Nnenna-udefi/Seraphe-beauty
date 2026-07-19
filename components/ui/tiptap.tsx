36f266961cbd191ea1646818a5c401fb6d9ba57f
Nnenna udefi <miriamudefi@gmail.com>
2026-07-19 22:41:45 +0100

    feat: add features to tiptap editor

--- components/ui/tiptap.tsx
+++ components/ui/tiptap.tsx
@@ -20,6 +20,36 @@ export default function Tiptap({ value, onChange }: TiptapProps) {
 
   return (
     <div className="border rounded-lg p-3 min-h-60">
+      <div className="flex gap-2 border-b p-2">
+        <button
+          type="button"
+          onClick={() => editor?.chain().focus().toggleBold().run()}
+        >
+          Bold
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor?.chain().focus().toggleItalic().run()}
+        >
+          Italic
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor?.chain().focus().toggleBulletList().run()}
+        >
+          • List
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor?.chain().focus().toggleOrderedList().run()}
+        >
+          1. List
+        </button>
+      </div>
+
       <EditorContent editor={editor} />
     </div>
   );
