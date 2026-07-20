7061aded1d77b03a9e5ffb4f6fbfa94d93ce4c32
Nnenna Udefi <miriamudefi@gmail.com>
2026-07-20 23:20:40 +0100

    Update index.tsx

--- components/shop/index.tsx
+++ components/shop/index.tsx
@@ -46,7 +46,7 @@ const ShopSeraphe = ({ products }: { products: Product[] }) => {
           </h2>
           <div className="py-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
             {products.map((product) => (
-              <div key={product._id} className="flex flex-col gap-1">
+              <div key={product.name} className="flex flex-col gap-1">
                 <div className=" pt-2">
                   <Image
                     src={product.images[0]}
@@ -64,7 +64,7 @@ const ShopSeraphe = ({ products }: { products: Product[] }) => {
                   <p className=" text-darkText text-sm py-2">
                     {product.shortDescription}
                   </p>
-                  <Link href={`product/${product._id}`}>
+                  <Link href={`product/${product.slug}`}>
                     <Button className="w-full">{product.price}</Button>
                   </Link>
                 </div>
